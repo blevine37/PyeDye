8405f1bb9799b94fba2502c503cb89d2fb6e8b7f
Benjamin Levine <levine@localhost.localdomain>
2021-02-08 19:23:41 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name="pyedye",
+      version='0.0'
+      description='Software for estimating fluorescence quantum yields',
+      author='Benjamin G. Levine',
+      url='https://github.com/blevine37/PyeDye',
+      packages=['pyedye'],
+      requires['numpy','h5py']
+      )
